8a1f629d231ceef6b6a00baec606fa2a1ad1fc95
NKLam <khoalamvn@gmail.com>
2023-10-02 02:23:46 +1100

    Add zooming camera for build mode

--- src/ui/main_screen/main_ui.gd
+++ src/ui/main_screen/main_ui.gd
@@ -2,6 +2,9 @@ extends Control
 
 @export var ship_sprite: Sprite2D
 @export var ship_grid: Node2D
+@export var camera: Camera2D
+@export var far_view_marker: Marker2D
+@export var space_background: Sprite2D
 
 @onready var build_show_toggle: MarginContainer = $BuildShowToggle
 @onready var build_menu: MarginContainer = $BuildMenu
@@ -14,12 +17,19 @@ func _ready() -> void:
 
 func _on_build_button_pressed():
 	if anim_player.animation_finished:
+		var tween = get_tree().create_tween()
 		if build_menu_open:
 			anim_player.play("hide_build_menu")
 			build_menu_open = false
+			ship_grid.visible = false
+			tween.tween_property(camera, "zoom", Vector2(0.1, 0.1), 0.5).set_trans(Tween.TRANS_LINEAR)
+			tween.tween_property(camera, "global_position", far_view_marker.global_position, 0.5).set_trans(Tween.TRANS_LINEAR)
 		else:
 			anim_player.play("show_build_menu")
 			build_menu_open = true
+			ship_grid.visible = true
+			tween.tween_property(camera, "global_position", ship_sprite.global_position, 0.5).set_trans(Tween.TRANS_LINEAR)
+			tween.tween_property(camera, "zoom", Vector2(0.5, 0.5), 0.5).set_trans(Tween.TRANS_LINEAR)
 
 
 func _on_play_dialog_pressed():
@@ -27,6 +37,7 @@ func _on_play_dialog_pressed():
 	ship_grid.visible = false
 	build_show_toggle.visible = false
 	build_menu.visible = false
+	space_background.visible = false
 	var dialog = Dialogic.start(EventManager.get_random_event())
 
 
@@ -37,5 +48,9 @@ func _on_dialogic_signal(arg: String):
 			ship_grid.visible = true
 			build_show_toggle.visible = true
 			build_menu.visible = true
-
+			space_background.visible = true
+			if build_menu_open:
+				ship_grid.visible = true
+			else:
+				ship_grid.visible = false
 
---
+++ src/ui/ship_building/grid_placement.gd
@@ -0,0 +1,94 @@
+extends Node2D
+class_name GridPlacement
+
+@export var tilemap: Node
+# @export var grid_size: Vector2
+
+var building
+
+var mouse_pos: Vector2
+var placement_coord: Vector2
+var original_placement_coord: Vector2
+var preview_pos: Vector2
+var current_building: Node
+var previous_rotation = 0
+
+var rotate_counter = 0
+
+
+func _ready():
+	BuildingManager.building_selected.connect(_building_button_pressed)
+
+
+func get_new_building():
+	var new_building = building.instantiate()
+	new_building.preview = true
+	new_building.rotation = previous_rotation
+	new_building.visible = true
+	add_child(new_building)
+	current_building = new_building
+
+
+func _physics_process(_delta):
+	if building == null or current_building == null:
+		return
+
+	mouse_pos = get_global_mouse_position()
+	mouse_pos = tilemap.to_local(mouse_pos)
+	placement_coord = tilemap.local_to_map(mouse_pos)
+	original_placement_coord = placement_coord
+	if rotate_counter == 1:
+		placement_coord.x -= 1
+	elif rotate_counter == 2:
+		placement_coord -= Vector2(1, 1)
+	elif rotate_counter == 3:
+		placement_coord.y -= 1
+	preview_pos = tilemap.map_to_local(placement_coord) + Vector2(32, 32)
+	preview_pos = tilemap.to_global(preview_pos)
+
+	if current_building:
+		current_building.outside_gridmap = is_outside_gridmap(original_placement_coord)
+		current_building.visible = !current_building.outside_gridmap
+		current_building.global_position = preview_pos
+		
+		if Input.is_action_just_pressed("ui_cancel"):
+			current_building.queue_free()
+			current_building = null
+
+		if Input.is_action_just_pressed("place_building"):
+			if not current_building.collider.has_overlapping_areas():
+				if not is_outside_gridmap(placement_coord):
+					place_building()
+
+		if Input.is_action_just_pressed("rotate_cw"):
+			current_building.rotation += PI/2
+			rotate_counter = (rotate_counter + 1) % 4
+		elif Input.is_action_just_pressed("rotate_ccw"):
+			current_building.rotation -= PI/2
+
+
+func is_outside_gridmap(coord: Vector2) -> bool:
+	if tilemap.get_cell_source_id(0, coord) == -1:
+		return true
+	else:
+		return false
+
+
+func place_building():
+	current_building.preview = false
+	previous_rotation = current_building.rotation
+	current_building = null
+	get_new_building()
+
+
+func stop_building_preview():
+	if current_building != null:
+		previous_rotation = current_building.rotation
+		current_building.queue_free()
+		current_building = null
+
+
+func _building_button_pressed(_building: PackedScene):
+	building = _building
+	stop_building_preview()
+	get_new_building()
