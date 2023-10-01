d3ba2aac59cbe31b86c17979aec99df88dac22d4
Jack McCaffrey <jackmccaffrey96@gmail.con>
2023-10-01 17:08:13 +0100

    Replaced `self_modulate` calls with `modulate` to fix grid placement colours

--- src/buildings/building.gd
+++ src/buildings/building.gd
@@ -50,18 +50,18 @@ func set_building_placed():
 
 func set_original_color():
 	if sprite is Sprite2D:
-		original_color = sprite.self_modulate
+		original_color = sprite.modulate
 	elif sprite is Node2D and sprite.get_child_count() > 0:
-		original_color = sprite.get_children()[0].self_modulate
+		original_color = sprite.get_children()[0].modulate
 
 
 func color_sprite(r, g, b, a):
 	if sprite is Sprite2D:
-		sprite.self_modulate = Color(r, g, b, a)
+		sprite.modulate = Color(r, g, b, a)
 	elif sprite is Node2D and sprite.get_child_count() > 0:
 		# This is a group of rectangle, used in testing only
 		for item in sprite.get_children():
-			item.self_modulate = Color(r, g, b, a)
+			item.modulate = Color(r, g, b, a)
 
 
 func _notification(what: int) -> void:
