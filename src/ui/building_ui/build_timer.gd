370a2b347b76a6f13ac3e1f2ebd909c246a3f407
Jack McCaffrey <jackmccaffrey96@gmail.con>
2023-10-03 14:51:27 +0100

    Fixed build timer placement/rotation

--- src/ui/building_ui/build_timer.gd
+++ src/ui/building_ui/build_timer.gd
@@ -1,4 +1,13 @@
 extends Control
 
-@onready var label = $CenterContainer/HBoxContainer/Label
+@onready var icon = $CenterContainer/BuildTimerUI/CenterContainer/HBoxContainer/TextureRect
+@onready var label = $CenterContainer/BuildTimerUI/CenterContainer/HBoxContainer/Label
+
+
+func _ready():
+	self.pivot_offset = self.size / 2
+
+
+func _process(delta):
+	self.rotation = get_parent().rotation * -1
 
