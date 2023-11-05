7c8fc600cc0dd0345432b8bb45b7b1a7dcc6b319
NKLam <khoalamvn@gmail.com>
2023-11-05 16:16:48 +1100

    Make planet title logo randomize

--- src/ui/menus/main/main_menu.gd
+++ src/ui/menus/main/main_menu.gd
@@ -1,12 +1,16 @@
 extends Control
 
 @onready var setting_menu = $SettingMenu
+@onready var planet_logo: Sprite2D = $PlanetLogo
 
 @export var scene_after_start: PackedScene
+@export var planet_images: Array[Texture2D]
 
 var started = false
 
 func _ready():
+	var i = randi_range(0, len(planet_images) - 1)
+	planet_logo.texture = planet_images[i]
 	var music = load("res://assets/audio/music/ld54-bgm-medley-no-alarms-1.1.mp3")
 	SoundManager.play_music(music, 0.2, "Music")
 	ScreenTransitionManager.fade_in(0.8)
