28f8cc4569e77f174602abee81d7f9e705aec23a
NKLam <khoalamvn@gmail.com>
2023-11-27 03:05:30 +1100

    Add prerequisite to upgrades unlock

--- src/ui/command_screen/research_tab.gd
+++ src/ui/command_screen/research_tab.gd
@@ -11,10 +11,30 @@ func reset_stuff_on_tab() -> void:
 	upgrade_button.visible = false
 	update_status_all_upgrade_buttons()
 	
-func chose_an_upgrade(upgrade_id: EnumAutoload.UpgradeId):
-	if upgrade_id in ResourceManager.current_upgrades:
+func select_an_upgrade(upgrade_data: TechUpgradeButton):
+	upgrade_desc_label.text = "[u]{upgrade_name}[/u]\n[font_size=12]{upgrade_description}[/font_size]".format(
+		{"upgrade_name": upgrade_data.upgrade_name, "upgrade_description": upgrade_data.upgrade_description})
+
+	# Check if already activated
+	if upgrade_data.upgrade_id != EnumAutoload.UpgradeId.NONE and \
+		upgrade_data.upgrade_id in ResourceManager.current_upgrades:
+		selected_upgrade_id = EnumAutoload.UpgradeId.NONE
+		upgrade_button.visible = false
 		return
-	selected_upgrade_id = upgrade_id
+
+	# Check for prerequisite upgrade
+	var have_previous_upgrade = true
+	if len(upgrade_data.require_one_of_these_upgrades) > 0:
+		have_previous_upgrade = false
+		for upgr in upgrade_data.require_one_of_these_upgrades:
+			if upgr.activated:
+				have_previous_upgrade = true
+	if not have_previous_upgrade:
+		selected_upgrade_id = EnumAutoload.UpgradeId.NONE
+		upgrade_button.visible = false
+		return
+
+	selected_upgrade_id = upgrade_data.upgrade_id
 	upgrade_button.visible = true
 
 func update_status_all_upgrade_buttons():
@@ -26,7 +46,7 @@ func update_status_all_upgrade_buttons():
 
 func _on_upgrade_button_pressed() -> void:
 	if selected_upgrade_id != EnumAutoload.UpgradeId.NONE:
-		# TODO: Add upgrade cost here
+		# TODO: Add upgrade cost here. If not enough, return
 		ResourceManager.current_upgrades.append(selected_upgrade_id)
 	update_status_all_upgrade_buttons()
 	upgrade_button.visible = false
