aed48b2433c1880143a76725c616541f34877165
NKLam <khoalamvn@gmail.com>
2023-12-20 12:28:38 +1100

    Update the music volume slider

--- src/managers/fmod_manager/fmod_manager.gd
+++ src/managers/fmod_manager/fmod_manager.gd
@@ -1,11 +1,14 @@
 extends Node
 
 @export var event: EventAsset
+@export var bus_asset: BusAsset
 var instance: EventInstance = null
+var bus: Bus = null
 
 func _ready() -> void:
     instance = FMODRuntime.create_instance(event)
     instance.start()
+    bus = FMODStudioModule.get_studio_system().get_bus(bus_asset.path)
 
 func _notification(what: int) -> void:
     match what:
@@ -20,5 +23,9 @@ func on_predelete() -> void:
 func update_dynamic_music(new_ratio: float):
     if instance == null:
         return
-
     instance.set_parameter_by_name("Food-Water-Air", new_ratio, false)
+
+func change_music_volume(volume: float):
+    if bus == null:
+        return
+    bus.set_volume(volume)
